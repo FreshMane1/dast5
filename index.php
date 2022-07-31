669b3e8e89f6d91841150e8899fc4919207e0b7d
FreshMane1 <108697596+FreshMane1@users.noreply.github.com>
2022-07-31 15:41:45 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,478 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>PopWeb</title>
+    <meta name="description" content="Описание страницы" />
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="restangle">
+    </div>
+
+    <div class="restangle-2">
+    </div>
+
+    <div class="restangle-3">
+    </div>
+        
+    <div class="restangle-4">
+    </div>
+
+    <div class="restangle-5">
+    </div>
+
+    <div class="restangle-6">
+    </div>
+
+    <div class="restangle-8">
+    </div>
+
+        <div class="restangle-7">
+    </div>
+
+    <div class="restangle-10"></div>
+
+    <div class="restangle-9"></div>
+
+    <div class="restangle-11"></div>
+
+    <div class="restangle-100"></div>
+
+    <div class="restangle-down"></div>
+
+    <div class="restangle-20"></div>
+
+    <div class="restangle-30"></div>
+
+    <div class="restangle-90"></div>
+
+        <div class="restangle-91"></div>
+
+            <div class="restangle-92"></div>
+
+                <div class="restangle-93"></div>
+
+                    <div class="restangle-94"></div>
+
+                        <div class="restangle-95"></div>
+
+    <div class="restangle-1000"></div>
+
+    <div class="restangle-1001"></div>
+
+    <div class="restangle-1002"></div>
+
+    <div class="restangle-1003"></div>
+
+    <div class="restangle-1004"></div>
+
+    <div class="restangle-1005"></div>
+
+    <div class="header-main-restangle"></div>
+
+    <div class="header-main-restangle-2"></div>
+
+    <div class="restangle-2000"></div>
+
+    <div class="restangle-2001"></div>
+
+    <div class="restangle-2002"></div>
+
+    <div class="restangle-2003"></div>
+
+    <div class="restangle-2004"></div>
+
+    <div class="restangle-2005"></div>
+
+    <div class="restangle-2006"></div>
+
+    <div class="restangle-2007"></div>
+
+
+    <div class="photo-main">
+      <div class="macbook"><img src="img/macbook.png"></div>
+      <div class="pngwing"><img src="img/pngwing.png"></div>
+      <div class="star">
+        <img src="img/Star.png"></div>
+      </div>
+
+      <div class="nft-photo">
+        <img src="img/nft.png">
+      </div>
+
+      <div class="starbucks-photo">
+        <img src="img/starbucks.png">
+      </div>
+
+      <div class="pizza-photo">
+        <img src="img/pizza.png">
+      </div>
+
+      <div class="down-photo">
+        <img src="img/down.png">
+      </div>
+
+
+      <div class="dior-photo">
+        <img src="img/dior.png">
+      </div>
+
+      <div class="down-photos">
+        <img class="photo-1" src="img/1.png">
+        <img class="photo-2" src="img/2.png">
+        <img class="photo-3" src="img/3.png">
+        <img class="photo-4" src="img/4.png">
+        <img class="photo-5" src="img/5.png">
+        <img class="photo-6" src="img/6.png">
+      </div>
+
+    </div>
+    <div class="header">
+            <div class="popweb">
+              PopWeb
+            </div>
+            <a class="main" href="pages/main.php">Главная</a>
+            <a class="prices" href="pages/price.php">Стоимость</a>
+            <a class="portfolio" href="pages/portfolio.php">Портфолио</a>
+            <button class="support">Контакты</button>
+            <a class="about" href="pages/about.php">О студии</a>
+            <div class="main-buttons">
+              <a class="add-btn" href="#">ЗАКАЗАТЬ</a>
+              <a class="add-btn2" href="#">Заказать сайт</a>
+            </div>
+            <div class="title">
+              Закажите<br>Удобный Сайт<br>Под Ключ
+            </div>
+            <div class="subtitle">
+              Разработаем качественный и быстрый<br> сайт за короткие сроки, при заказе<br>SEO-оптимизация в подарок!
+              <div class="arrow">
+                <img src="img/arrow.png">
+              </div>
+              <div class="text-main">Узнать подробнее</div>
+
+
+              <div class="last-projects">Последние Проекты</div>
+              <div class="nft-landing">Landing Page для курса по NFT</div>
+              <div class="nft-title">Одностраничный сайт для продажи онлайн-курса<br> по NFT, выполненный в красно-ораньжевых тонах</div>
+              <div><a class="watch" href="#">Смотреть полностью</a></div>
+              
+              <div class="starbucks-landing">Концепт Landing Page для StarBucks</div>
+              <div class="starbucks-title">Концепт одностраничного сайта для презентации<br>нового кафе от StarBucks</div>
+              <div><a class="watch-1" href="#">Смотреть полностью</a></div>
+
+              <div class="pizza-landing">Landing Page для пиццерии</div>
+              <div class="pizza-title">Одностраничный сайт для интернет-пиццерии<br> с возможностью заказа еды онлайн</div>
+              <div><a class="watch-2" href="#">Смотреть полностью</a></div>
+
+              <div class="dior-landing">Концепт интернет-магазина Dior</div>
+              <div class="dior-title">Инфа о данном супер пупер проекте плюс цели задачи дизайна кратно</div>
+              <div><a class="watch-3" href="#">Смотреть полностью</a></div>
+
+              <div class="dribble-landing">Dribble-Шоты</div>
+              <div class="dribble-title">Текст че такоедрибл шоты и блчять вообще похуй лол шатал мамнал хуй май пу медведь гризли</div>
+              <div><a class="watch-4" href="#">Смотреть полностью</a></div>
+
+            </div>
+
+              <div class="work">
+
+                <div class="work-text">
+                  Наш подход<br> к созданию сайтов
+                </div>
+
+                <div class="subtext">Мы делаем профессионально,<br>качественно и ответственно.
+                </div>
+
+                <div class="subtext-1">Мы создаем уникальные сайты, основываясь<br> на пожеланиях клиента и современных<br> трендах web-дизайна и маркетинга.
+                </div>
+
+                <div class="subtext-2">В нашей команде опытные дизайнеры и<br> разработчики, готовые воплотить любую<br> вашу идею в качественый сайт.</div>
+
+                <div class="subtext-3">Мы заботимся о ваших проектах, ваших<br> сроках и ваших нервах, несмотря ни на что,<br> и это обещание.</div>
+              </div>
+
+              <div class="our-work">
+              
+              <div class="main-text">
+                Спектр Услуг
+              </div>
+
+              <div class="main-title">
+                Landing Page
+              </div>
+
+              <div class="main-title-1">
+                Многостраничный сайт
+              </div>
+
+              <div class="main-title-2">
+                Интернет-Магазин
+              </div>
+
+              <div class="main-title-3">
+                Квиз-сайт
+              </div>
+
+              <div class="main-title-4">
+                Редизайн сайта
+              </div>
+
+              <div class="main-title-5">
+                SEO-Продвижение
+              </div>
+
+              <div class="main-subtitle">
+                Одностраничный сайт, обычно<br> направленный на презентацию<br> конкретного продукта/проекта
+              </div>
+
+              <div class="main-subtitle-1">
+                Сайт, имеющий множество<br> дочерних страниц, подходит для<br> многих типов бизнеса
+              </div>
+
+              <div class="main-subtitle-2">
+                Сайт, имеющий возможность<br> покупки товаров онлайн
+              </div>
+
+              <div class="main-subtitle-3">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-subtitle-4">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-subtitle-5">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-price">
+                <div class="ot">от</div>
+                <div class="price">14,000₽</div>
+              </div>
+
+              <div class="main-price-1">
+                <div class="ot-1">от</div>
+                <div class="price-1">24,000₽</div>
+              </div>
+
+              <div class="main-price-2">
+                <div class="ot-2">от</div>
+                <div class="price-2">32,000₽</div>
+              </div>
+
+              <div class="main-price-3">
+                <div class="ot-3">от</div>
+                <div class="price-3">12,000₽</div>
+              </div>
+
+              <div class="main-price-4">
+                <div class="ot-4">от</div>
+                <div class="price-4">10,000₽</div>
+              </div>
+
+              <div class="main-price-5">
+                <div class="ot-5">от</div>
+                <div class="price-5">10,000₽</div>
+
+            <a class="zakazat" href="#">Заказать сайт</a>
+            <a class="zakazat1" href="#">Заказать сайт</a>
+            <a class="zakazat2" href="#">Заказать сайт</a>
+            <a class="zakazat3" href="#">Заказать сайт</a>
+            <a class="zakazat4" href="#">Заказать сайт</a>
+            <a class="zakazat5" href="#">Заказать сайт</a>
+              </div>
+
+
+
+              </div>
+
+              <div class="header-main">
+                <div class="header-title">Этапы Работы</div>
+                <div class="header-subtitle">Для создания качественных сайтов<br>
+мы проделываем следующие этапы работы:</div>
+
+              <div class="main-header-title">
+                №1 Интервью
+              </div>
+
+              <div class="main-header-title-1">
+                №2 Структура сайта
+              </div>
+
+              <div class="main-header-title-2">
+                №3 Создание мудборда
+              </div>
+
+              <div class="main-header-title-3">
+                №4 Прототип сайта
+              </div>
+
+              <div class="main-header-title-4">
+                №5 Дизайн cайта
+              </div>
+
+              <div class="main-header-title-5">
+                №6 Верстка
+              </div>
+
+              <div class="main-header-title-6">
+                №7 Хостинг + домен
+              </div>
+
+              <div class="main-header-title-7">
+                №8 Защита проекта
+              </div>
+
+              <div class="main-header-subtitle">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-header-subtitle-1">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-header-subtitle-2">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-header-subtitle-3">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-header-subtitle-4">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-header-subtitle-5">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-header-subtitle-6">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+              <div class="main-header-subtitle-7">
+                В нашей команде опытные<br> дизайнеры и разработчики,<br> готовые воплотить любую вашу<br> идею в качественый сайт.
+              </div>
+
+
+              </div>
+
+              <div class="foot">
+                <div class="footer-title">
+                  Сотрудничая<br>
+с Нами, Вы Получите:
+                </div>
+
+                <div class="restangle-foot"></div>
+                <div class="restangle-foot1"></div>
+                <div class="restangle-foot2"></div>
+
+                <div class="thing-photo">
+                  <img src="img/thing.png">
+                </div>
+                <div class="ellipse-1">
+                  <img src="img/ellipse-1.png">
+                </div>
+
+                <div class="tasks-photo">
+                  <img src="img/tasks.png">
+                </div>
+                <div class="ellipse">
+                  <img src="img/ellipse.png">
+                </div>
+
+                <div class="calc-photo">
+                  <img src="img/calc.png">
+                </div>
+                <div class="ellipse-1">
+                  <img src="img/ellipse-1.png">
+                </div>
+
+                <div class="footer-title-1">
+                  Уникальный дизайн
+                </div>
+
+                <div class="footer-title-2">Сильная верстка</div>
+
+                <div class="footer-title-3">Оптимизация сайта</div>
+
+                 <div class="footer-subtitle">
+                  Никаких шаблонов! Мы делаем<br> все сайты индивидуальными,<br> отвечающие на все пожелания<br> любого заказчика
+                </div>
+
+                <div class="footer-subtitle-1">
+                  Никаких конструкторов и<br> прочих платформ, которые<br> замедляют сайт! В разработке<br> мы используем только код 
+                </div>
+
+                <div class="footer-subtitle-2">Адаптируем сайты под все<br> устройства, базовое SEO-<br>продвижение  </div>
+
+                <div class="ellipse-2">
+                  <img src="img/ellipse-1.png">
+                </div>
+
+                <div class="foot-restangle"></div>
+                <button class="zakazat-site">Заказать сайт</button>
+
+
+              </div>
+
+              <div class="contentss">
+
+                <div class="content-title">Наша Цель</div>
+
+                <div class="content-subtitle">Мы заботимся о ваших проектах, ваших<br> сроках и ваших нервах, несмотря ни на<br> что, и это обещание, мы заботимся о ваших<br> проектах, ваших сроках и ваших нервах,<br> несмотря ни на что, и это обещание.</div>
+
+                <div class="content-subtitle-1">Мы заботимся о ваших проектах, ваших<br> сроках и ваших нервах, несмотря ни на<br> что, и это обещание, мы заботимся о ваших<br> проектах, ваших сроках и ваших нервах,<br> несмотря ни на что, и это обещание.</div>
+                <div class="content-photo">
+                  <img src="img/content-photo.png">
+                </div>
+
+                <div class="restangle-cont"></div>
+
+                <div class="content-star">
+                  <img src="img/content star.png">
+                </div>
+                <div class="end-title">Свяжитесь с Нами</div>
+
+                <div class="zayavka">Заполните заявку, наш менеджер<br> свяжется с вами в ближайшее время</div>
+
+                <div class="messanger">Или напишите сейчас в личные<br> сообщения любой из данных соцсетей</div>
+
+
+                <div class="white-messanger"></div>
+
+                <div class="white-messanger1"></div>
+
+                <div class="white-messanger2"></div>
+
+                <div class="messanger-restangle"></div>
+
+              </div>
+
+  </div>
+      
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+            </div>
+
+
+    </div>
+    <footer>
+      авторские права
+    </footer>
+    <script type="text/javascript">
+      document.querySelector('.support').onclick = () => {
+    window.scrollTo(pageYOffset, 2000);
+}
+    </script>
+  </body>
+</html>
